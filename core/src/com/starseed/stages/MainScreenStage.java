74fa082b360f5dbbd04516b6b009762709c6527f
Marija Dragojevic <marija.dragojevic88@gmail.com>
2016-01-15 21:14:44 +0100

    into animation added.

--- core/src/com/starseed/stages/MainScreenStage.java
+++ core/src/com/starseed/stages/MainScreenStage.java
@@ -1,5 +1,8 @@
 package com.starseed.stages;
 
+import java.util.HashMap;
+import java.util.Random;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -27,42 +30,63 @@ public class MainScreenStage extends Stage {
 	private Ship player1 = null;
 	private Ship player2 = null;
 	private float time;
-	private int actionID;
+	private int actionID=0;
+	private Label variableLabel=null;
+	HashMap<Integer,Float> actionMap;
 	public MainScreenStage(MainScreen mainScreen) {
 		super(new FitViewport(
 			  Constants.APP_WIDTH, Constants.APP_HEIGHT ));
 		this.mainScreen = mainScreen;
 		style = UIStyle.getSingleton();
 		setUpMainStage();
-		time = 0.5f;
-		actionID = 0;
+		actionMap = new HashMap<Integer,Float>();
+		defineActionMap();
+		
 	}
-	
+	private void defineActionMap() {
+		actionMap.put(0, 0.75f);  // fire seed
+		actionMap.put(1, 0.5f);  // go up
+		actionMap.put(2, 0.5f);  // stop
+		actionMap.put(3, 0.5f);  // go left
+		actionMap.put(4, 0.5f);  // stop going left, position: pi
+		actionMap.put(5, 0.75f);  // fire lasers
+		actionMap.put(6, 0.5f);  // go down
+		actionMap.put(7, 0.5f);  // stop
+		actionMap.put(8, 0.5f);  // go right
+		actionMap.put(9, 0.5f);  // stop going right, position: 0
+		time = -1.0f;
+	}
 	public void setUpMainStage() {
 		world = WorldUtils.createWorld();
 		addActor(new Background());
 		addRocketButtons();
-		addLabel("Space seed INC.", 50, Color.WHITE,
+		addLabel("Space seed INC.", 52, Color.WHITE,
 				 100, 680, true);
 		addLabel(generateSubtitleText(), 30, Color.WHITE,
 				 130, 630, false);
 		String credits = "Coders:\n    Bruno Mikus\n    Marija Dragojevic\nArtist:\n    Ivana Berkovic\n\nlibGDX JAM: January 2016";
-		addLabel(credits, 30, Color.WHITE,
+		addLabel(credits, 28, Color.WHITE,
 				 130, 50, false);
+		String var_text = "Player 1:  W          Player 2:  Up";
+		variableLabel = addLabel(var_text, 28, Color.WHITE, 580, 400, false);
 		setUpShips();
 	}
 	
     private void setUpShips() {
-        player1 = new Ship( WorldUtils.createPlayerShip(world, new Vector2(Constants.WORLD_WIDTH * 0.65f, Constants.WORLD_HEIGHT * 0.15f), 0f), 1 );
+        player1 = new Ship( WorldUtils.createPlayerShip(world, new Vector2(Constants.WORLD_WIDTH * 0.65f, Constants.WORLD_HEIGHT * 0.1f), 0f), 1 );
         addActor(player1);
-        player1.setEngineOn(true);
-        player2 = new Ship( WorldUtils.createPlayerShip(world, new Vector2(Constants.WORLD_WIDTH * 0.85f, Constants.WORLD_HEIGHT * 0.15f), 0f), 2 );
+        player2 = new Ship( WorldUtils.createPlayerShip(world, new Vector2(Constants.WORLD_WIDTH * 0.85f, Constants.WORLD_HEIGHT * 0.1f), 0f), 2 );
         addActor(player2);
-        player2.setEngineOn(true);
     }
 	
 	private String generateSubtitleText() {
-		return "Subtitle";
+		String texts [] = {"Resistance is futile. Your solar system will be inseminated.",
+						   "Bringing life to you starborhood.",
+						   "Need for seed.",
+						   "Every seed is needed in your starborhood.",
+						   "Every seed is sacred."};
+		
+		return texts[new Random().nextInt(texts.length)];
 	}
 	
 	private void addRocketButtons() {
@@ -122,46 +146,69 @@ public class MainScreenStage extends Stage {
 	}
 	
 	public void changeAction(int actionID) {
+		String var_text;
 		switch (actionID) {
 		case 0:
 			player1.getBody().setTransform(player1.getBody().getPosition(), 0f);
 			player1.getBody().setAngularVelocity(0f);
 			player2.getBody().setTransform(player2.getBody().getPosition(), 0f);
 			player2.getBody().setAngularVelocity(0f);
-			player1.setEngineOn(true);
-			player2.setEngineOn(true);
+			player1.setEngineOn(false);
+			player2.setEngineOn(false);
+			var_text = "Player 1:  Q          Player 2:  Shift";
+			variableLabel.setText(var_text);
 			break;
 		case 1:
+			player1.setEngineOn(true);
+			player2.setEngineOn(true);
+			var_text = "Player 1:  W          Player 2:  Up";
+			variableLabel.setText(var_text);
+			break;
+		case 2:
 			player1.setEngineOn(false);
 			player2.setEngineOn(false);
 			break;
-		case 2:
+		case 3:
 			player1.setTurnLeft(true);
 			player2.setTurnLeft(true);
+			var_text = "Player 1:  A          Player 2:  Left";
+			variableLabel.setText(var_text);
 			break;
-		case 3:
+		case 4:
 			player1.setTurnLeft(false);
 			player2.setTurnLeft(false);			
 			break;
-		case 4:
+		case 5:
 			player1.getBody().setTransform(player1.getBody().getPosition(), (float)Math.PI);
 			player1.getBody().setAngularVelocity(0f);
 			player2.getBody().setTransform(player2.getBody().getPosition(), (float)Math.PI);
 			player2.getBody().setAngularVelocity(0f);
+			player1.setEngineOn(false);
+			player2.setEngineOn(false);
+			var_text = "Player 1:  Q          Player 2:  Shift";
+			variableLabel.setText(var_text);
+			break;
+		case 6:
 			player1.setEngineOn(true);
 			player2.setEngineOn(true);
+			var_text = "Player 1:  W          Player 2:  Up";
+			variableLabel.setText(var_text);
 			break;
-		case 5:
+		case 7:
 			player1.setEngineOn(false);
 			player2.setEngineOn(false);
 			break;
-		case 6:
+		case 8:
 			player1.setTurnRight(true);
 			player2.setTurnRight(true);
+			var_text = "Player 1:  D          Player 2:  Right";
+			variableLabel.setText(var_text);
 			break;
-		case 7:
+		case 9:
 			player1.setTurnRight(false);
 			player2.setTurnRight(false);
+			break;
+		default:
 			break;
 		}
 	}
@@ -170,13 +217,13 @@ public class MainScreenStage extends Stage {
 	public void act(float delta) {
 		super.act(delta);
 		time -= delta;
-		if (time < 0.0f) {
-			time = 0.5f;
+		if (time < 0f) {
+			time = actionMap.get(actionID);
+			changeAction(actionID);
 			actionID++;
-			if (actionID > 7) {
+			if (actionID > 9) {
 				actionID = 0;
 			}
-			changeAction(actionID);
 		}
 		// Fixed timestep
 		accumulator += delta;

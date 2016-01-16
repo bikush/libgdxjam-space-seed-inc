f310c47c169ffe246f11f07659054cc5b64acd36
Bruno Mikus <bikush.mruno@gmail.com>
2016-01-16 12:27:09 +0100

    Removed a warning from UIStyle.

--- core/src/com/starseed/stages/UIStyle.java
+++ core/src/com/starseed/stages/UIStyle.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFont
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.starseed.util.Constants;
 
 public class UIStyle {

67adde0dc46b8a4f8d72c74d23fa535972b7c6b1
Bruno Mikus <bikush.mruno@gmail.com>
2016-01-17 20:48:10 +0100

    Fixed button sound.

--- core/src/com/starseed/util/Constants.java
+++ core/src/com/starseed/util/Constants.java
@@ -105,7 +105,7 @@ public class Constants {
      */
     
 	public static final String MAIN_SOUND_FILE = "sounds/teru_-_Velvet_Sun.ogg";
-	public static final String BUTTON_PRESSED = "sounds/button_pressed.wav";
+	public static final String BUTTON_PRESSED = "sounds/button_pressed.ogg";
 	public static final String SOUND_ASTEROID_DESTROY = "sounds/asteroid_boom.ogg";
 	public static final String[] SOUND_ASTEROID_HIT = { 
 			"sounds/asteroid_hit_1.ogg", 
